e88583ddd3bd1b7f4012d2bef90e03f8b64355f9
Will Charczuk <will@blendlabs.com>
2017-01-04 13:15:17 -0800

    events!

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	util "github.com/blendlabs/go-util"
@@ -10,11 +11,30 @@ import (
 
 func simulate(router simulation.Router) {
 	println()
-	println(router.Name(), "Starting 10 second simulation")
+
 	sim := simulation.New(router)
+	sim.SetSimulationLength(16 * time.Second)
 	sim.SetServerCount(8)
-	sim.SetServerWorkerCount(8)
-	sim.SetCachedResourceFetchDuration(30 * time.Millisecond)
+	sim.SetServerWorkerCount(256)
+	sim.SetCachedResourceCount(1 << 10)
+	sim.SetCachedResourceFetchDuration(16 * time.Millisecond)
+
+	// double number of servers
+	sim.AddEvent(8*time.Second, func(s *simulation.Simulation) {
+		fmt.Printf("%v :: Doubling server count\n", time.Now().Sub(s.Started))
+		for x := 0; x < s.ServerCount(); x++ {
+			s.Servers = append(s.Servers, s.CreateServer())
+		}
+		s.Router.SetServers(s.Servers)
+	})
+	sim.AddEvent(12*time.Second, func(s *simulation.Simulation) {
+		fmt.Printf("%v :: Killing a (random) server\n", time.Now().Sub(s.Started))
+		randomIndex := rand.Intn(len(s.Servers))
+		s.Servers = append(s.Servers[0:randomIndex], s.Servers[randomIndex+1:]...)
+		s.Router.SetServers(s.Servers)
+	})
+
+	println(router.Name(), "Starting", fmt.Sprintf("%v", sim.SimulationLength()), "simulation")
 	sim.Run()
 
 	var totalTimes []time.Duration
@@ -43,13 +63,18 @@ func simulate(router simulation.Router) {
 			misses++
 		}
 	}
+
+	totalRequests := len(sim.Requests) - (notRouted + notCompleted)
+
 	println(router.Name(), "Simulation Results")
-	fmt.Printf("Throughput %0.2f rps\n", float64(len(sim.Requests))/float64(sim.SimulationLength()/time.Second))
-	fmt.Printf("Average Total Time %v\n", util.Math.MeanOfDuration(totalTimes))
-	fmt.Printf("Average Work Time %v\n", util.Math.MeanOfDuration(workTimes))
-	fmt.Printf("Average Routing Time %v\n", util.Math.MeanOfDuration(routingTimes))
-	fmt.Printf("Not Routed %d, Not Completed %d\n", notRouted, notCompleted)
-	fmt.Printf("Cache Miss Rate %d/%d ~= %0.2f%%\n", misses, len(sim.Requests), float64(misses)/float64(len(sim.Requests))*100)
+	println("---------------------------------")
+	fmt.Printf("Throughput           : %0.2f rps\n", float64(len(sim.Requests))/float64(sim.SimulationLength()/time.Second))
+	fmt.Printf("Average Routing Time : %v\n", util.Math.MeanOfDuration(routingTimes))
+	fmt.Printf("Average Work Time    : %v\n", util.Math.MeanOfDuration(workTimes))
+	fmt.Printf("Average Total Time   : %v\n", util.Math.MeanOfDuration(totalTimes))
+	fmt.Printf("Not Routed           : %d\n", notRouted)
+	fmt.Printf("Not Completed        : %d\n", notCompleted)
+	fmt.Printf("Cache Miss Rate %d/%d ~= %0.2f%%\n", misses, totalRequests, float64(misses)/float64(totalRequests)*100)
 }
 
 func main() {
--- simulation.go
+++ simulation.go
@@ -2,6 +2,7 @@ package simulation
 
 import (
 	"math/rand"
+	"sort"
 	"time"
 
 	util "github.com/blendlabs/go-util"
@@ -11,10 +12,24 @@ import (
 // New returns a new simulation.
 func New(router Router) *Simulation {
 	return &Simulation{
+		Events: collections.NewRingBuffer(),
 		abort:  make(chan bool),
 		Router: router,
 	}
 }
+
+type simulationEvent struct {
+	At     time.Duration
+	Action func(*Simulation)
+}
+
+type simulationEvents []interface{}
+
+func (se simulationEvents) Len() int { return len(se) }
+func (se simulationEvents) Less(a, b int) bool {
+	return se[a].(simulationEvent).At < se[b].(simulationEvent).At
+}
+func (se simulationEvents) Swap(a, b int) { se[a], se[b] = se[b], se[a] }
 
 // Simulation is the collection of state for the whole simulation.
 type Simulation struct {
@@ -22,16 +37,26 @@ type Simulation struct {
 	Servers   []*Server
 	Resources []string
 	Requests  []*Request
+	Events    *collections.RingBuffer
 
-	simulationLength               time.Duration
-	serverCount                    int
-	serverWorkerCount              int
-	serverIdentifierLength         int
-	cachedResourceCount            int
-	cachedResourceIdentifierLength int
-	cachedResourceFetchDuration    time.Duration
+	Started time.Time
+	Stopped time.Time
+
+	simulationLength            time.Duration
+	serverCount                 int
+	serverWorkerCount           int
+	cachedResourceCount         int
+	cachedResourceFetchDuration time.Duration
 
 	abort chan bool
+}
+
+// AddEvent adds an event to the simulation at a given offset from start.
+func (s *Simulation) AddEvent(at time.Duration, action func(*Simulation)) {
+	events := s.Events.AsSlice()
+	events = append(events, simulationEvent{At: at, Action: action})
+	sort.Sort(simulationEvents(events))
+	s.Events = collections.NewRingBufferFromSlice(events)
 }
 
 // SimulationLength is the length of the simulation.
@@ -47,6 +72,7 @@ func (s *Simulation) SetSimulationLength(length time.Duration) {
 	s.simulationLength = length
 }
 
+// ServerCount is the number of servers.
 func (s Simulation) ServerCount() int {
 	if s.serverCount == 0 {
 		return 8
@@ -54,10 +80,12 @@ func (s Simulation) ServerCount() int {
 	return s.serverCount
 }
 
+// SetServerCount sets the number of servers.
 func (s *Simulation) SetServerCount(count int) {
 	s.serverCount = count
 }
 
+// ServerWorkerCount is the number of goroutines per server.
 func (s Simulation) ServerWorkerCount() int {
 	if s.serverWorkerCount == 0 {
 		return 8
@@ -65,21 +93,12 @@ func (s Simulation) ServerWorkerCount() int {
 	return s.serverWorkerCount
 }
 
+// SetServerWorkerCount sets the number of goroutines per server.
 func (s *Simulation) SetServerWorkerCount(count int) {
 	s.serverWorkerCount = count
 }
 
-func (s Simulation) ServerIdentifierLength() int {
-	if s.serverIdentifierLength == 0 {
-		return 4
-	}
-	return s.serverIdentifierLength
-}
-
-func (s *Simulation) SetServerIdentifierLength(length int) {
-	s.serverIdentifierLength = length
-}
-
+// CachedResourceCount is the number of cached resources the system tracks.
 func (s Simulation) CachedResourceCount() int {
 	if s.cachedResourceCount == 0 {
 		return 1 << 10
@@ -87,24 +106,20 @@ func (s Simulation) CachedResourceCount() int {
 	return s.cachedResourceCount
 }
 
+// SetCachedResourceCount sets the cached resource count.
 func (s *Simulation) SetCachedResourceCount(count int) {
 	s.cachedResourceCount = count
 }
 
-func (s Simulation) CachedResourceIdentifierLength() int {
-	if s.cachedResourceIdentifierLength == 0 {
-		return 8
-	}
-	return s.cachedResourceIdentifierLength
-}
-
-func (s *Simulation) CachedResourceFetchDuration() time.Duration {
+// CachedResourceFetchDuration is the time it takes to complete a cache miss.
+func (s Simulation) CachedResourceFetchDuration() time.Duration {
 	if s.cachedResourceFetchDuration == 0 {
 		return 20 * time.Millisecond
 	}
 	return s.cachedResourceFetchDuration
 }
 
+// SetCachedResourceFetchDuration sets the cache miss penalty.
 func (s *Simulation) SetCachedResourceFetchDuration(fetchDuration time.Duration) {
 	s.cachedResourceFetchDuration = fetchDuration
 }
@@ -113,26 +128,26 @@ func (s *Simulation) SetCachedResourceFetchDuration(fetchDuration time.Duration)
 func (s *Simulation) Initialize() {
 	var servers []*Server
 	for x := 0; x < s.ServerCount(); x++ {
-		servers = append(servers, s.newServer())
+		servers = append(servers, s.CreateServer())
 	}
 	s.Router.SetServers(servers)
 	s.Servers = servers
 	s.Resources = s.newResourceSet()
 }
 
-// NewServer creates a new server.
-func (s Simulation) newServer() *Server {
+// CreateServer creates a new server.
+func (s Simulation) CreateServer() *Server {
 	return &Server{
-		ID:        util.String.RandomString(s.ServerIdentifierLength()),
+		ID:        util.UUIDv4().ToShortString(),
 		Resources: collections.NewSetOfString(),
-		Requests:  make(chan *Request, 32),
+		Requests:  make(chan *Request, s.ServerWorkerCount()),
 	}
 }
 
 func (s Simulation) newResourceSet() []string {
 	var resources []string
 	for x := 0; x < s.CachedResourceCount(); x++ {
-		resources = append(resources, util.String.RandomString(s.CachedResourceIdentifierLength()))
+		resources = append(resources, util.UUIDv4().ToShortString())
 	}
 	return resources
 }
@@ -150,12 +165,15 @@ func (s *Simulation) createRequest() *Request {
 	return req
 }
 
-func (s *Simulation) arrival() {
-	req := s.createRequest()
-	req.Arrival = time.Now()
-	server := s.Router.Route(req)
-	req.Routed = time.Now()
-	server.Requests <- req
+func (s *Simulation) processEvent() {
+	if s.Events.Len() == 0 {
+		return
+	}
+	event := s.Events.Peek().(simulationEvent)
+	if event.At < time.Now().Sub(s.Started) {
+		event.Action(s)
+		s.Events.Dequeue()
+	}
 }
 
 // Run starts and stops the simulation.
@@ -172,22 +190,39 @@ func (s *Simulation) Start() {
 		server.Run(s.abort)
 	}
 
-	// the below is essentially a firehose of requests
-	// we try to saturate the system to see what the
-	// throughput at max is.
+	// arrivals
 	go func() {
 		for {
 			select {
 			case <-s.abort:
 				return
 			default:
-				s.arrival()
+				req := s.createRequest()
+				req.Arrival = time.Now()
+				server := s.Router.Route(req)
+				req.Routed = time.Now()
+				server.Requests <- req
 			}
 		}
 	}()
+
+	// events
+	go func() {
+		for {
+			select {
+			case <-s.abort:
+				return
+			default:
+				s.processEvent()
+			}
+		}
+	}()
+
+	s.Started = time.Now()
 }
 
 // Stop stops the simulation.
 func (s *Simulation) Stop() {
 	s.abort <- true
+	s.Stopped = time.Now()
 }
